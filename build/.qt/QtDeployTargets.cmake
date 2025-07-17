84ae1ef15bac1d24631fe0296ee1294ae695570a
Axther <kevinmanso06@gmail.com>
2025-07-16 21:11:22 -0400

    Refactor CMake configuration to switch source and header file paths from 'include/' to 'src/'. Updated target link libraries to use 'PRIVATE' visibility for Qt dependencies. Removed obsolete MSVC-specific files and updated CMakeCache to reflect new compiler paths and settings.

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Chatty_FILE C:/Users/kevin/Desktop/Chatty/build/bin/Chatty.exe)
+set(__QT_DEPLOY_TARGET_Chatty_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Chatty_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
---
+++ build/CMakeFiles/Chatty.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chatty_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Chatty_autogen.dir/ParseCache.txt"
+  "Chatty_autogen"
+  "CMakeFiles/Chatty.dir/Chatty_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Chatty.dir/Chatty_autogen/mocs_compilation.cpp.obj.d"
+  "bin/Chatty.exe"
+  "bin/Chatty.exe.manifest"
+  "bin/Chatty.pdb"
+  "libChatty.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chatty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
